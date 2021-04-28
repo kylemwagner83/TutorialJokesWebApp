166de61a69489cc65fc24f946e9c0f2c4685d59b
Kyle Wagner <70072537+kylemwagner83@users.noreply.github.com>
2021-04-28 09:46:04 -0600

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutorialJokesWebApp.Models
+{
+    public class Joke
+    {
+        private int _Id;
+        private string _JokeQuestion;
+        private string _JokeAnswer;
+
+        public int Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+
+        public string JokeQuestion
+        {
+            get { return _JokeQuestion; }
+            set { _JokeQuestion = value; }
+        }
+
+        public string JokeAnswer
+        {
+            get { return _JokeAnswer; }
+            set { _JokeAnswer = value; }
+        }
+
+        public Joke()
+        {
+            
+        }
+
+    }
+}
